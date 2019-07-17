0e9758264c8d619d83803512429249c1297629b2
Cristian Perez <cristiangp777@gmail.com>
2019-07-17 14:40:54 -0700

    bad

---
+++ spec/unit/navbar.spec.js
@@ -0,0 +1,14 @@
+import Navbar from "../../src/components/Navbar.js"
+
+describe(Navbar, function() {
+  it("should be defined", () => {
+    expect(Navbar).toBeDefined();
+console.log(Navbar);
+  });
+  it("should be defined", () => {
+    expect(body).toContain("Home");
+
+  });
+
+
+});
