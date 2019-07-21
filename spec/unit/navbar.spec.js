1f2516bc63f3751da14634f8373780ebe5df7083
Cristian Perez <cristiangp777@gmail.com>
2019-07-20 17:02:02 -0700

    testing problems

--- spec/unit/navbar.spec.js
+++ spec/unit/navbar.spec.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import Navbar from "../../src/components/Navbar.js";
- import { BrowserRouter } from 'react-router-dom';
+ import {Link, MemoryRouter } from 'react-router-dom';
 import renderer from 'react-test-renderer';
 
 
-describe(Navbar, function() {
-  it("should be defined", () => {
-    expect(Navbar).toBeDefined();
 
-  });
   test('should contain appropriate links', () => {
     const component = renderer.create(
-  <BrowserRouter>
+  <MemoryRouter>
       <Navbar/>
-  </BrowserRouter>
+  </MemoryRouter>
     );
-    let tree = component.toJSON();
-    expect(tree).toContain("Farms");
+    let instance = component.root;
+    console.log(instance);
+    const farmLink = instance.find(
+        (el) => el.type === Link
+            && el.text === "Farms"
+
+    );
+
+
+
   });
-
-
-});
