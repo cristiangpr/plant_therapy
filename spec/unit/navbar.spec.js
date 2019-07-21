1d321ed04f23cd12ae31c0c152125b3d3bcc4184
Cristian Perez <cristiangp777@gmail.com>
2019-07-20 18:57:11 -0700

    temp test working

--- spec/unit/navbar.spec.js
+++ spec/unit/navbar.spec.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import Navbar from "../../src/components/Navbar.js";
- import {Link, MemoryRouter } from 'react-router-dom';
+ import { MemoryRouter } from 'react-router-dom';
 import renderer from 'react-test-renderer';
 
 
+describe(Navbar, function() {
+  it("should be defined", () => {
+    expect(Navbar).toBeDefined();
 
+  });
   test('should contain appropriate links', () => {
     const component = renderer.create(
   <MemoryRouter>
       <Navbar/>
   </MemoryRouter>
     );
-    let instance = component.root;
-    console.log(instance);
-    const farmLink = instance.find(
-        (el) => el.type === Link
-            && el.text === "Farms"
+    let treeString = JSON.stringify(component);
 
-    );
-
-
-
+    expect(treeString).toContain("Farms");
   });
+
+
+});
