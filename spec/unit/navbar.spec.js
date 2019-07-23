a62820a800188e1374275875e4b9e02831d75f1b
Cristian Perez <cristiangp777@gmail.com>
2019-07-23 11:26:42 -0700

    7/23

--- spec/unit/navbar.spec.js
+++ spec/unit/navbar.spec.js
@@ -5,19 +5,24 @@ import renderer from 'react-test-renderer';
 
 
 describe(Navbar, function() {
-  it("should be defined", () => {
-    expect(Navbar).toBeDefined();
-
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(
+          <MemoryRouter>
+            <Navbar/>
+        </MemoryRouter>)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
   });
-  test('should contain appropriate links', () => {
+  test('should contain appropriate links for page', () => {
     const component = renderer.create(
   <MemoryRouter>
       <Navbar/>
   </MemoryRouter>
     );
-    let treeString = JSON.stringify(component);
-
-    expect(treeString).toContain("Farms");
+  const allListItems = component.root.findAll(element => element.type === "li")
+console.log(allListItems);
+    expect(allListItems).toContain("Farms");
   });
 
 
